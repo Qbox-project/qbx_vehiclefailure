44be8f35c6c446c993557efc9b5ab0444c10bb3a
GhzGarage <qbcore@programmer.net>
2021-04-18 13:58:35 -0500

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-VehicleFailure'
+version '1.0.0'
+
+client_scripts {
+	"config.lua",
+	"client.lua"
+}
+
+server_scripts {
+	"config.lua",
+	"server.lua"
+}
