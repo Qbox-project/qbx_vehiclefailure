add57b2044418ba46ad0c9a50a3ee3803d3fa9a1
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-12-18 11:03:20 -0500

    chore: file organization (#11)
    * chore: file organization

--- server/main.lua
+++ server/main.lua
@@ -4,33 +4,33 @@ lib.addCommand('fix', {help = 'Repair your vehicle (Admin Only)', restricted = '
 end)
 
 exports.qbx_core:CreateUseableItem("repairkit", function(source, item)
-    local player = QBCore.Functions.GetPlayer(source)
+    local player = exports.qbx_core:GetPlayer(source)
 	if player.Functions.GetItemBySlot(item.slot) ~= nil then
         TriggerClientEvent("qb-vehiclefailure:client:RepairVehicle", source)
     end
 end)
 
 exports.qbx_core:CreateUseableItem("cleaningkit", function(source, item)
-    local player = QBCore.Functions.GetPlayer(source)
+    local player = exports.qbx_core:GetPlayer(source)
 	if player.Functions.GetItemBySlot(item.slot) ~= nil then
         TriggerClientEvent("qb-vehiclefailure:client:CleanVehicle", source)
     end
 end)
 
 exports.qbx_core:CreateUseableItem("advancedrepairkit", function(source, item)
-    local player = QBCore.Functions.GetPlayer(source)
+    local player = exports.qbx_core:GetPlayer(source)
 	if player.Functions.GetItemBySlot(item.slot) ~= nil then
         TriggerClientEvent("qb-vehiclefailure:client:RepairVehicleFull", source)
     end
 end)
 
 RegisterNetEvent('qb-vehiclefailure:removeItem', function(item)
-    local player = QBCore.Functions.GetPlayer(source)
+    local player = exports.qbx_core:GetPlayer(source)
     player.Functions.RemoveItem(item, 1)
 end)
 
 RegisterNetEvent('qb-vehiclefailure:server:removewashingkit', function(veh)
-    local player = QBCore.Functions.GetPlayer(source)
+    local player = exports.qbx_core:GetPlayer(source)
     player.Functions.RemoveItem("cleaningkit", 1)
     TriggerClientEvent('qb-vehiclefailure:client:SyncWash', -1, veh)
 end)
